238b659e1636d31f26c989b2c229e99bdadd2ca5
nikhil <sdr957@users.noreply.github.com>
2021-02-23 11:03:09 +0530

    third commit

--- resume/urls.py
+++ resume/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from core import views
-from django.urls import path,include
+from django.urls import path,include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
